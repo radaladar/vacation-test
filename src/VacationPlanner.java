026efe78c1974e3a38b81ea8dfa3cdf6c5fde956
radaladar <bbiczok@gmail.com>
2017-02-27 13:03:26 +0100

    removed useless named test, added new multi-dependency test, algorithm basically works, have to write checks for malformed input

--- src/VacationPlanner.java
+++ src/VacationPlanner.java
@@ -1,18 +1,29 @@
 import java.util.ArrayList;
 
 public class VacationPlanner {
-
+    
     public static String plan(ArrayList<String> route) {
         StringBuilder plannedRoute = new StringBuilder("");
         for (String destination : route) {
             String currentDest = destination.split(" => ")[0];
+            if (destination.split(" => ").length > 1) {
+                String prevDest = destination.split(" => ")[1];
+                if (plannedRoute.toString().contains(currentDest)
+                && !plannedRoute.toString().contains(prevDest)) {
+                    plannedRoute.insert(plannedRoute.toString().indexOf(currentDest), prevDest);
+                }
+                else if (plannedRoute.toString().contains(prevDest)
+                        && !plannedRoute.toString().contains(currentDest)) {
+                    plannedRoute.insert(plannedRoute.toString().indexOf(prevDest)+1, currentDest);
+                }  else {
+                    plannedRoute.append(prevDest);
+                    plannedRoute.append(currentDest);
+                }
+            }
             if (!plannedRoute.toString().contains(currentDest)) {
                 plannedRoute.append(currentDest);
             }
-            if (destination.split(" => ").length > 1) {
-                String prevDest = destination.split(" => ")[1];
-                plannedRoute.insert(plannedRoute.toString().indexOf(currentDest), prevDest);
-            }
+
         }
         return plannedRoute.toString();
     }
--- test/VacationPlannerTest.java
+++ test/VacationPlannerTest.java
@@ -9,9 +9,9 @@ public class VacationPlannerTest {
 
     ArrayList<String> onePlace = new ArrayList<>();
     ArrayList<String> simpleRoute = new ArrayList<>();
-    ArrayList<String> simpleRouteWithNames = new ArrayList<>();
-    ArrayList<String> oneDependeny = new ArrayList<>();
+    ArrayList<String> oneDependency = new ArrayList<>();
     ArrayList<String> multipleDependencies = new ArrayList<>();
+    ArrayList<String> multipleDependencies2 = new ArrayList<>();
 
     @Before
     public void setup() {
@@ -21,13 +21,9 @@ public class VacationPlannerTest {
         simpleRoute.add("y => ");
         simpleRoute.add("z => ");
 
-        simpleRouteWithNames.add("London => ");
-        simpleRouteWithNames.add("Paris => ");
-        simpleRouteWithNames.add("Rome => ");
-
-        oneDependeny.add("x => ");
-        oneDependeny.add("y => z");
-        oneDependeny.add("z => ");
+        oneDependency.add("x => ");
+        oneDependency.add("y => z");
+        oneDependency.add("z => ");
 
         multipleDependencies.add("u => ");
         multipleDependencies.add("v => w");
@@ -35,15 +31,22 @@ public class VacationPlannerTest {
         multipleDependencies.add("x => u");
         multipleDependencies.add("y => v");
         multipleDependencies.add("z => ");
+
+        multipleDependencies2.add("u => w");
+        multipleDependencies2.add("v => x");
+        multipleDependencies2.add("w => y");
+        multipleDependencies2.add("x => ");
+        multipleDependencies2.add("y => ");
+        multipleDependencies2.add("z => u");
     }
     @Test
     public void testOnePlace() {assertEquals("x", VacationPlanner.plan(onePlace));}
     @Test
     public void testSimpleRoute() {assertEquals("xyz", VacationPlanner.plan(simpleRoute));}
     @Test
-    public void testSimpleRouteWithNames() {assertEquals("LondonParisRome", VacationPlanner.plan(simpleRouteWithNames));}
-    @Test
-    public void testOneDependency() {assertEquals("xzy", VacationPlanner.plan(oneDependeny));}
+    public void testOneDependency() {assertEquals("xzy", VacationPlanner.plan(oneDependency));}
     @Test
     public void testMultipleDependencies() {assertEquals("uxzwvy", VacationPlanner.plan(multipleDependencies));}
+    @Test
+    public void testMultipleDependencies2() {assertEquals("ywuzxv", VacationPlanner.plan(multipleDependencies2));}
 }
