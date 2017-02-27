2391c7ec27cc675adf647fc46190acebcff053c9
radaladar <bbiczok@gmail.com>
2017-02-27 12:29:01 +0100

    four tests pass

--- src/VacationPlanner.java
+++ src/VacationPlanner.java
@@ -5,7 +5,14 @@ public class VacationPlanner {
     public static String plan(ArrayList<String> route) {
         StringBuilder plannedRoute = new StringBuilder("");
         for (String destination : route) {
-            plannedRoute.append(destination.split(" => ")[0]);
+            String currentDest = destination.split(" => ")[0];
+            if (!plannedRoute.toString().contains(currentDest)) {
+                plannedRoute.append(currentDest);
+            }
+            if (destination.split(" => ").length > 1) {
+                String prevDest = destination.split(" => ")[1];
+                plannedRoute.insert(plannedRoute.toString().indexOf(currentDest), prevDest);
+            }
         }
         return plannedRoute.toString();
     }
