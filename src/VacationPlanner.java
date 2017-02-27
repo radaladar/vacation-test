d636e362e1bccaef93c2e1b470e0fb088d7b1d87
radaladar <bbiczok@gmail.com>
2017-02-27 11:33:03 +0100

    two tests pass

--- src/VacationPlanner.java
+++ src/VacationPlanner.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class VacationPlanner {
 
     public static String plan(ArrayList<String> route) {
-        return route.get(0).substring(0,1);
+        StringBuilder plannedRoute = new StringBuilder("");
+        for (String destination : route) {
+            plannedRoute.append(destination.substring(0,1));
+        }
+        return plannedRoute.toString();
     }
 }
